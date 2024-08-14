cbf2138d3030cc7393920de52600befbedd9619a
Gustavo Godoi da Silva <gustavogodoi772@gmail.com>
2024-08-14 19:15:43 -0300

    criando projeto

---
+++ src/test/java/br/com/fiap/sphereapi/SphereapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.sphereapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SphereapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
